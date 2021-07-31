8120d6a03d9fed00acac9ac6b90b728d65e44747
unknown <bhardwajsiddhant03@gmail.com>
2021-07-31 20:21:08 +0530

    Added Actor Based Movi Recommender

--- app.py
+++ app.py
@@ -28,60 +28,110 @@ def index():
         for i,val in enumerate(name):
             name[i] = val[0].upper() + val[1:].lower()
         name = '+'.join(name)
-        print(name)
-        response = requests.get("http://api.themoviedb.org/3/search/person?query={name}e&api_key=6dbe3c2b48ba7012390499fe4e59c6a4")
-        response = response.json()
-        if response:
-            print(response)
-            return render_template("500.html")
+        #response = requests.get("http://api.themoviedb.org/3/search/person?query={name}&api_key=6dbe3c2b48ba7012390499fe4e59c6a4")
+        #response = response.json()
+        response_1 = requests.get("https://api.themoviedb.org/3/search/movie?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&query=" + name)
+        response_1 = response_1.json()
+        if len(response_1['results']) > 0:
+            try:
+                res = second_recommender.second_recommender(response_1['results'][0]['original_title'])
+                if res == "No Movies":
+                    movie_name = response_1['results'][0]
+                    movie_name['release_date'] = movie_name['release_date'][:4]
+                    movie_name['genre_ids'] = list(set(movie_name['genre_ids']))
+                    movie_name['genre_ids'] = [genre_d[movie_name['genre_ids'][j]] for j in range(len(movie_name['genre_ids'])) ]
+                    movie_id = movie_name['id']
+                    recommend = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&language=en-US&page=1")
+                    recommend = recommend.json()
+                    arr = recommend['results'][:9]
+                    for i in arr:
+                        i['genre_ids'] = list(set(i['genre_ids']))
+                        i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
+                        i['release_date'] = i['release_date'][:4]
+                    return render_template("index.html",users = "None",arr = arr,first = movie_name,upcoming = "None")
+                else:
+                    response = res
+                    arr = []
+                    for name in response:
+                        request1 = requests.get("https://api.themoviedb.org/3/search/movie?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&query=" + name)
+                        request1 = request1.json()
+                        arr.append(request1['results'][0])
+                    movie_name = response_1['results'][0]
+                    movie_name['release_date'] = movie_name['release_date'][:4]
+                    movie_name['genre_ids'] = list(set(movie_name['genre_ids']))
+                    movie_name['genre_ids'] = [genre_d[movie_name['genre_ids'][j]] for j in range(len(movie_name['genre_ids'])) ]
+                    for i in arr:
+                        i['genre_ids'] = list(set(i['genre_ids']))
+                        i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
+                        i['release_date'] = i['release_date'][:4]
+                    return render_template("index.html",users = arr,first = movie_name, upcoming = "None")
+            except:
+                actor = requests.get(f"http://api.themoviedb.org/3/search/person?query={name}&api_key=6dbe3c2b48ba7012390499fe4e59c6a4")
+                actor = actor.json()
+                if len(actor['results']) > 0:
+                    try:
+                        return actor_recommender(name)
+                    except:
+                        return render_template("500.html")
+                else:
+                    return render_template("500.html")
         else:
-            response_1 = requests.get("https://api.themoviedb.org/3/search/movie?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&query=" + name)
-            response_1 = response_1.json()
-            if len(response_1['results']) > 0:
+            actor = requests.get(f"http://api.themoviedb.org/3/search/person?query={name}&api_key=6dbe3c2b48ba7012390499fe4e59c6a4")
+            actor = actor.json()
+            if len(actor['results']) > 0:
                 try:
-                    res = second_recommender.second_recommender(response_1['results'][0]['original_title'])
-                    if res == "Movie not":
-                        movie_name = response_1['results'][0]
-                        movie_name['release_date'] = movie_name['release_date'][:4]
-                        movie_name['genre_ids'] = list(set(movie_name['genre_ids']))
-                        movie_name['genre_ids'] = [genre_d[movie_name['genre_ids'][j]] for j in range(len(movie_name['genre_ids'])) ]
-                        movie_id = movie_name['id']
-                        recommend = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&language=en-US&page=1")
-                        recommend = recommend.json()
-                        arr = recommend['results'][:9]
-                        for i in arr:
-                            i['genre_ids'] = list(set(i['genre_ids']))
-                            i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
-                            i['release_date'] = i['release_date'][:4]
-                        return render_template("index.html",users = "None",arr = arr,first = movie_name,upcoming = "None")
-                    else:
-                        response = res
-                        arr = []
-                        for name in response:
-                            request1 = requests.get("https://api.themoviedb.org/3/search/movie?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&query=" + name)
-                            request1 = request1.json()
-                            arr.append(request1['results'][0])
-                        movie_name = response_1['results'][0]
-                        movie_name['release_date'] = movie_name['release_date'][:4]
-                        movie_name['genre_ids'] = list(set(movie_name['genre_ids']))
-                        movie_name['genre_ids'] = [genre_d[movie_name['genre_ids'][j]] for j in range(len(movie_name['genre_ids'])) ]
-                        for i in arr:
-                            i['genre_ids'] = list(set(i['genre_ids']))
-                            i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
-                            i['release_date'] = i['release_date'][:4]
-                        return render_template("index.html",users = arr,first = movie_name, upcoming = "None")
+                    return actor_recommender(name)
                 except:
                     return render_template("500.html")
             else:
                 return render_template("index.html",users = "None1",first = "None",upcoming = "None")
-    response = requests.get("https://api.themoviedb.org/3/movie/upcoming?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&language=en-US&page=1")
-    response = response.json()
-    response = response['results'][:9]
-    for i in response:
+    else:
+        response = requests.get("https://api.themoviedb.org/3/movie/upcoming?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&language=en-US&page=1")
+        response = response.json()
+        response = response['results'][:9]
+        for i in response:
+            i['genre_ids'] = list(set(i['genre_ids']))
+            i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
+            i['release_date'] = i['release_date'][:4]
+        return render_template("index.html",users = "None",first = "None", upcoming = response)
+    
+def actor_recommender(name):
+    google_search = requests.get(f"https://serpapi.com/search.json?engine=google&q={name}&api_key=27957cb0d044df28115a38f0dc6ffc6e2702d76f01f377832762c2ed9774be18")
+    google_search = google_search.json()
+    option_one = "movies"
+    option_two = "movies_and_tv_shows"
+    if option_one in google_search['knowledge_graph']:
+        movies = [i['name'] for i in google_search['knowledge_graph'][option_one]]
+    elif option_two in google_search['knowledge_graph']:
+        movies = [i['name'] for i in google_search['knowledge_graph'][option_two]]
+    co_actors = [i['name'] for i in google_search['knowledge_graph']['people_also_search_for']]
+    for co_actor in co_actors:
+        search = requests.get(f"https://serpapi.com/search.json?engine=google&q={co_actor}&api_key=27957cb0d044df28115a38f0dc6ffc6e2702d76f01f377832762c2ed9774be18")
+        search = search.json()
+        if 'type' in search['knowledge_graph']:
+            if 'actor' in search['knowledge_graph']['type'].lower() or 'actress' in search['knowledge_graph']['type'].lower():
+                if option_one in search['knowledge_graph']:
+                    movies += [j['name'] for j in search['knowledge_graph'][option_one]]
+                elif option_two in search['knowledge_graph']:
+                    movies += [j['name'] for j in search['knowledge_graph'][option_two]]
+        else:
+            continue
+    arr = []
+    for i in movies:
+        res_2 = requests.get(f"https://api.themoviedb.org/3/search/movie?api_key=6dbe3c2b48ba7012390499fe4e59c6a4&query={i}")
+        res_2 = res_2.json()
+        if len(res_2['results']) > 0:
+            arr.append(res_2['results'][0])
+    print('Gotcha 2')
+    for i in arr:
         i['genre_ids'] = list(set(i['genre_ids']))
         i['genre_ids'] = [genre_d[i['genre_ids'][j]] for j in range(len(i['genre_ids'])) ]
         i['release_date'] = i['release_date'][:4]
-    return render_template("index.html",users = "None",first = "None", upcoming = response)
+    movie_name = arr[0]
+    arr = arr[1:-1]
+    print('Gotcha')
+    return render_template("index.html",users = arr,first = movie_name,upcoming = "None")
+   
 
 @app.route('/movie',methods=['GET'])
 def recommend_movies():
